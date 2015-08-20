b830082644e571e7cfc8ad3511be626c69ba9ba9
Shaun Chua <shon3005@gmail.com>
2015-08-19 18:31:57 -0700

    first commit

---
+++ K9-Chemistry/K9-Chemistry/EditProfileViewController.swift
@@ -0,0 +1,190 @@
+//
+//  EditProfileViewController.swift
+//  K9-Chemistry
+//
+//  Created by Shaun Chua on 1/8/15.
+//  Copyright (c) 2015 Shaun Chua. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var profile: Profile?
+    
+    let nameField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let usernameField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let websiteField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let aboutMeField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let emailField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let phoneField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    let genderField = UITextField(frame: CGRect(x: 0, y: 8, width: 100, height: 30))
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        println(self.profile)
+        println(self.profile?.name)
+        println(self.profile?.website)
+        println(self.profile?.aboutMe)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        button.frame = CGRectMake(250, 75, 100, 30)
+        //button.backgroundColor = UIColor.greenColor()
+        button.setTitle("Edit Photo", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let profile = self.profile {
+            if let name = profile.name {
+                nameField.text = name
+            }
+            if let username = profile.username {
+                usernameField.text = username
+            }
+            if let website = profile.website {
+                websiteField.text = website
+            }
+            if let aboutMe = profile.aboutMe {
+                aboutMeField.text = aboutMe
+            }
+            if let email = profile.email {
+                emailField.text = email
+            }
+            if let phone = profile.phone {
+                phoneField.text = phone
+            }
+            if let gender = profile.gender {
+                genderField.text = gender
+            }
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            
+            return 5
+        }
+        return 3
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        println("generate cell for \(indexPath)")
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        if indexPath.section == 0 {
+            
+            if indexPath.row == 0 {
+                nameField.placeholder = "Name"
+                cell.contentView.addSubview(nameField)
+                nameField.delegate = self
+            }
+            else if indexPath.row == 1 {
+                usernameField.placeholder = "Username"
+                cell.contentView.addSubview(usernameField)
+                usernameField.delegate = self
+            }
+            else if indexPath.row == 2 {
+                websiteField.placeholder = "Website"
+                cell.contentView.addSubview(websiteField)
+                websiteField.delegate = self
+            }
+            else if indexPath.row == 3 {
+                aboutMeField.placeholder = "About Me"
+                cell.contentView.addSubview(aboutMeField)
+                aboutMeField.delegate = self
+            }
+        } else {
+            if indexPath.row == 0 {
+                emailField.placeholder = "Email"
+                cell.contentView.addSubview(emailField)
+                emailField.delegate = self
+            }
+            else if indexPath.row == 1 {
+                phoneField.placeholder = "Phone"
+                cell.contentView.addSubview(phoneField)
+                phoneField.delegate = self
+            }
+            else if indexPath.row == 2 {
+                genderField.placeholder = "Gender"
+                cell.contentView.addSubview(genderField)
+                genderField.delegate = self
+            }
+            
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch(section) {
+        case 1: return "PRIVATE INFORMATION"
+            break
+        default: return ""
+            break
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField == self.nameField {
+            self.profile?.name = textField.text
+        }
+        else if textField == self.usernameField {
+            self.profile?.username = textField.text
+        }
+        else if textField == self.websiteField {
+            self.profile?.website = textField.text
+        }
+        else if textField == self.aboutMeField {
+            self.profile?.aboutMe = textField.text
+        }
+        else if textField == self.emailField {
+            self.profile?.email = textField.text
+        }
+        else if textField == self.phoneField {
+            self.profile?.phone = textField.text
+        }
+        else {
+            self.profile?.gender = textField.text
+        }
+    }
+    
+    func buttonAction(sender:UIButton!)
+    {
+        println("Button tapped")
+    }
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        //let indexPath = self.tableView.indexPathForCell(sender as! UITableViewCell)!
+//        //let profile = self.profile[indexPath.row]
+//        var destination = segue.destinationViewController as! FifthViewController
+//        destination.profile1 = profile
+//    }
+
+
+}
---
+++ K9-Chemistry/K9-Chemistry/FifthViewController.swift
@@ -0,0 +1,94 @@
+//
+//  FifthViewController.swift
+//  K9-Chemistry
+//
+//  Created by Shaun Chua on 20/7/15.
+//  Copyright (c) 2015 Shaun Chua. All rights reserved.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+    
+    var shon3005 = Profile(name: "Shaun Chua", username: "shon3005", website: "hello.me", aboutMe: "LOL", email: "shaun.chua@nyu.edu", phone: "(347) 302-0851", gender: "Male")
+    
+    let nameLabel = UILabel(frame: CGRect(x: 8, y: 200, width: 100, height: 30))
+    let websiteLabel = UILabel(frame: CGRect(x: 8, y: 250, width: 100, height: 30))
+    let aboutMeLabel = UILabel(frame: CGRect(x: 8, y: 300, width: 100, height: 30))
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        println(self.shon3005)
+        println(self.shon3005.name)
+        println(self.shon3005.aboutMe)
+        println(self.shon3005.website)
+        nameLabel.text = shon3005.name
+        websiteLabel.text = shon3005.website
+        aboutMeLabel.text = shon3005.aboutMe
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(nameLabel)
+        //let name = shon3005.name
+        //nameLabel.text = name
+        
+        view.addSubview(websiteLabel)
+        //let website = shon3005.website
+        //websiteLabel.text = website
+        
+        view.addSubview(aboutMeLabel)
+        //let aboutMe = shon3005.aboutMe
+        //aboutMeLabel.text = aboutMe
+        
+        //if let profile1 = self.profile1 {
+            
+            //if let name = profile1.name {
+                //nameLabel.text = name
+            //}
+            //if let website = profile1.website {
+                //websiteLabel.text = website
+            //}
+            //if let aboutMe = profile1.aboutMe {
+                //aboutMeLabel.text = aboutMe
+            //}
+        
+        let name = shon3005.name
+        nameLabel.text = name
+        let website = shon3005.website
+        websiteLabel.text = website
+        let aboutMe = shon3005.aboutMe
+        aboutMeLabel.text = aboutMe
+        
+    }
+    
+     override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        //let indexPath = self.tableView.indexPathForCell(sender as! UITableViewCell)!
+        //let profile = self.profile[indexPath.row]
+        var destination = segue.destinationViewController as! EditProfileViewController
+        destination.profile = shon3005
+    }
+    
+    //func textFieldDidEndEditing(textLabel: UILabel) {
+        //if textLabel == self.nameLabel {
+            //self.profile1?.name = textLabel.text
+        //} else if textLabel == self.websiteLabel {
+            //self.profile1?.website == textLabel.text
+        //} else if textLabel == self.aboutMeLabel {
+            //self.profile1?.aboutMe == textLabel.text
+        //}
+    //}
+    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+//    }
+    
+}
